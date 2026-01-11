13548e54900e1cdb7e231b431a89a9110158408f
Marz2916 <akmalaiman314@gmail.com>
2026-01-11 21:57:29 +0800

    Refactor Checkout class with private field and methods

--- java-backend/src/main/java/com/kakigamerz/Checkout.java
+++ java-backend/src/main/java/com/kakigamerz/Checkout.java
@@ -3,14 +3,14 @@ package com.kakigamerz;
 import java.util.List;
 
 public class Checkout {
-    // 1. Private Field
+
     private List<CartItem> items;
 
-    // 2. Getter and Setter
+
     public List<CartItem> getItems() { return items; }
     public void setItems(List<CartItem> items) { this.items = items; }
 
-    // 3. Encapsulated Inner Class
+
     public static class CartItem {
         private long id;
         private String name;
