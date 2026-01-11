69dc073ef8a607087ea8a1cf496467dee7ee377b
Marz2916 <akmalaiman314@gmail.com>
2026-01-11 22:07:02 +0800

    Fix formatting in config.js for API_BASE_URL

--- react-frontend/src/config.js
+++ react-frontend/src/config.js
@@ -1,7 +1,7 @@
-// src/config.js
+
 
 const isLocal = window.location.hostname === "localhost";
 
 export const API_BASE_URL = isLocal
-    ? "http://localhost:8080" // If browser is on localhost, use local backend
-    : "https://kakigamerz-backend.onrender.com"; // If deployed, use Render backend
+    ? "http://localhost:8080" 
+    : "https://kakigamerz-backend.onrender.com"; 
