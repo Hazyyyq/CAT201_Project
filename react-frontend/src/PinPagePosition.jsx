5e7cb1d0d6257eb4e601208b8f372e8f1a4229aa
Marz2916 <akmalaiman314@gmail.com>
2026-01-11 22:06:33 +0800

    Fix scroll to top functionality on pathname change

--- react-frontend/src/PinPagePosition.jsx
+++ react-frontend/src/PinPagePosition.jsx
@@ -5,9 +5,9 @@ export default function ScrollToTop() {
     const {pathname} = useLocation();
 
     useEffect(() => {
-        //Scroll to the top left
+        
         window.scrollTo(0, 0);
-    }, [pathname]); // This runs every time (the URL) changes
+    }, [pathname]); 
 
     return null;
 }
